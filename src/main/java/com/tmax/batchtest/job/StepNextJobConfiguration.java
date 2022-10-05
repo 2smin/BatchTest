5e0524b08a0a23cd603205eb2f5bcf1d11f9f7ed
2smin <planner263@gmail.com>
2022-10-05 10:53:31 +0900

    init project

---
+++ src/main/java/com/tmax/batchtest/job/StepNextJobConfiguration.java
@@ -0,0 +1,56 @@
+package com.tmax.batchtest.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepNextJob(){
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>>>> this is step1");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step step2(){
+        return stepBuilderFactory.get("step2")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>>>> this is step2");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step step3(){
+        return stepBuilderFactory.get("step3")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>>>> this is step3");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+}
---
+++ src/test/java/com/tmax/batchtest/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.batchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
